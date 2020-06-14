b9ff48deff9187fd56de34536414055db1b05d30
Don-Stevenson <dsteven1@gmail.com>
2020-06-13 22:40:08 -0400

    update comments

--- exercises/src/exercises/core.clj
+++ exercises/src/exercises/core.clj
@@ -2,3 +2,9 @@
   (* x x))
 
 (square 4)
+
+;; ctrl alt c  then space      evaluate the highlighted form () * most useful
+;; 
+;; ctrl alt c  then e        evaluate a snippet of the form ()
+;; 
+;; ctr alt c then enter      evaluate the whole file * most useful
