0849d34c9288b8692fd2361480b464387475d4ed
Don-Stevenson <dsteven1@gmail.com>
2020-06-16 19:49:16 -0400

    working with classifying different types of triangles

--- exercises/src/exercises/ex3.clj
+++ exercises/src/exercises/ex3.clj
@@ -2,6 +2,14 @@
   [a b c]
   (and
    (< a (+ b c)) (< b (+ a c)) (< c (+ a b))))
+;; returning if the triangle is not valid, equilateral, isosceles, right Triangle or scalene
+(defn classifyTriangle
+  [a b c]
+  (cond (not (triangle? a b c)) :invalid
+        (= a b c) :equilateral
+        (or (= a b) (= b c) (= c a)) :isosceles
+        (or (= (* c c) (+ (* a a) (* b b)))) :right-Triangle
+        :else
+        :scalene))
 
-
-(triangle? 1 1 1)
+(classifyTriangle  4 3 5)
