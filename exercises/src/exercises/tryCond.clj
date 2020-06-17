a2ce4d678d5189453ec181d44990a0eea3c12d20
Don-Stevenson <dsteven1@gmail.com>
2020-06-17 13:58:16 -0400

    with three conditions

--- exercises/src/exercises/tryCond.clj
+++ exercises/src/exercises/tryCond.clj
@@ -3,9 +3,7 @@
 (defn isGreaterThanTwo
   [x]
   (cond
-    ;(not a number x)) :not-a-number
     (> x 2) :x-is-greater-than-two
     (= x 2) :x-is-equal-two-two
-    (< x 2) :x-is-less-than-two
-    :else  :x-is-not-a-number))
-(isGreaterThanTwo "a")
+    (< x 2) :x-is-less-than-two))
+(isGreaterThanTwo 4)
