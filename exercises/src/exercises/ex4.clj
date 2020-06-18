2534d1af0a9c2d2cd79bc2e65ca97db239765cd2
Don-Stevenson <dsteven1@gmail.com>
2020-06-18 19:51:02 -0400

    add given solution and test cases

--- exercises/src/exercises/ex4.clj
+++ exercises/src/exercises/ex4.clj
@@ -7,7 +7,16 @@
 
 (clamp 5 1 4)
 
+;; Given solution
+;; ***************
+; (defn clamp
+  ;[x min max]
+  ;(cond (<= min x max) x
+ ;       (< x min) min
+;        :else max))
 
+;; Test Cases
+;; ***********
 ;;(is (= 2 (clamp 2 1 4)))
 
 ;;(is (= 1 (clamp 0 1 4)))
