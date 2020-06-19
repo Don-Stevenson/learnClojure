7ddbc4c6b0e767680127e244f2b0155d64daceb1
Don-Stevenson <dsteven1@gmail.com>
2020-06-18 23:13:57 -0400

    working solution to finding max num in an array

--- exercises/src/exercises/ex5.clj
+++ exercises/src/exercises/ex5.clj
@@ -1,6 +1,8 @@
 (defn largest [nums]
-  (apply max (nums))
-  
-  )
+  (apply max nums))
 
-((= 3 (largest [1 2 -1 3 3 2 1 0])))
+;(apply max [1 2 3 4])
+
+;; tests
+;; *****
+(= 3 (largest [1 2 -1 3 3 2 1 0]))
