3468dea369d258705423abdb9f7966c7c88a638c
Don-Stevenson <dsteven1@gmail.com>
2020-06-18 23:18:56 -0400

    working with 2 solutions for finding max number

--- exercises/src/exercises/ex5.clj
+++ exercises/src/exercises/ex5.clj
@@ -1,8 +1,14 @@
+
+;; largest is a function that returns 
+;; the largest number in an array
 (defn largest [nums]
   (apply max nums))
 
-;(apply max [1 2 3 4])
+;; same as above but with reduce max
+(defn largest-reduce [nums]
+  (reduce max nums))
 
 ;; tests
 ;; *****
 (= 3 (largest [1 2 -1 3 3 2 1 0]))
+(largest-reduce [1 2 -1 3 3 2 1 0])
