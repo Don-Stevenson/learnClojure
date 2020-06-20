a24b70a83491fb679ad392a248431fc2ae5a9ec2
Don-Stevenson <dsteven1@gmail.com>
2020-06-20 11:35:56 -0400

    update comments and update variable

--- exercises/src/exercises/sumDice.clj
+++ exercises/src/exercises/sumDice.clj
@@ -1,11 +1,16 @@
 ;; Result of pair programming with Marco and Chris June 19 2020
 ;; ************************************************************
 
+;; creates a random number between 1 and 6
 (defn dice10 [x]
   (->> x
        (rand-int)
        (inc)))
 
+(dice10 6)
+
+;; loops through dice, prints the results and recurs until you get 6
+;; *****************************************************************
 (defn looped-dice
   [sides]
   (loop [results (dice10 sides)]
@@ -13,14 +18,20 @@
       "You got 6!"
       (do (println results) (recur (dice10 sides))))))
 
+;; rolls the number of dice depening on how many rolls given as a parameter
+;; ************************************************************************ 
 (defn list-of-dice
   [number-roll]
   (repeatedly number-roll #(dice10 6)))
 
+
+;; sums the dice total of the number of the dice rolled.
+;;  Prints the numbers rolled, then sums them
+;;  ***************************************** 
 (defn sum-dice
   [number-of-dice]
-  (let [l (list-of-dice number-of-dice)]
-    (println l)
-    (apply + l)))
+  (let [dice-list (list-of-dice number-of-dice)]
+    (println dice-list)
+    (apply + dice-list)))
 
 (sum-dice 10)
