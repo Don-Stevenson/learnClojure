4d5b202502aae1c8c78765e7616d23a54f2df2c9
Don-Stevenson <dsteven1@gmail.com>
2020-06-21 14:26:29 -0400

    add instructions in comments

--- exercises/src/exercises/ex2.clj
+++ exercises/src/exercises/ex2.clj
@@ -4,6 +4,7 @@
 ;;To check if 3 sides make a triangle, 
 ;;you need to check that every side is less than or 
 ;;equal to the sum of the other two sides.
+;;*************************************************
 
 (defn triangle?
   [a b c]
