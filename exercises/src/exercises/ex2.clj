432691f496fe389db2cafa6707110d6572237472
Don-Stevenson <dsteven1@gmail.com>
2020-06-21 14:20:44 -0400

    add instructions in comments

--- exercises/src/exercises/ex2.clj
+++ exercises/src/exercises/ex2.clj
@@ -1,3 +1,10 @@
+;;Write a function to determine if some 3 side lengths 
+;;are sufficient to make a triangle.
+
+;;To check if 3 sides make a triangle, 
+;;you need to check that every side is less than or 
+;;equal to the sum of the other two sides.
+
 (defn triangle?
   [a b c]
   (and
