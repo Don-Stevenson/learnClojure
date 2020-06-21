23a81e187bde90cf6892767c2521312a0786941e
Don-Stevenson <dsteven1@gmail.com>
2020-06-21 13:59:40 -0400

    setup ex 6

---
+++ exercises/src/exercises/ex6.clj
@@ -0,0 +1,16 @@
+;;scrabble score
+;;Mark as Finished
+;;Implement a function score that calculates the scrabble score for a given word.
+
+;;Use google to find out how much each letter is worth.
+
+
+(defn score
+  [word])
+
+
+(= 8 (score "hello"))
+
+(= 17 (score "question"))
+
+(= 35 (score "quizzed"))
