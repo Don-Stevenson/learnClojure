1c329407474015ca618336c6311bc907d6f286fc
Don-Stevenson <dsteven1@gmail.com>
2020-06-21 14:16:47 -0400

    with solution

--- exercises/src/exercises/ex6.clj
+++ exercises/src/exercises/ex6.clj
@@ -5,12 +5,40 @@
 ;;Use google to find out how much each letter is worth.
 
 
+;; setup points 
+;; ************
+(def points
+  {\a 1
+   \b 3
+   \c 3
+   \d 2
+   \e 1
+   \f 4
+   \g 2
+   \h 4
+   \i 1
+   \j 8
+   \k 5
+   \l 1
+   \m 3
+   \n 1
+   \o 1
+   \p 3
+   \q 10
+   \r 1
+   \s 1
+   \t 1
+   \u 1
+   \v 4
+   \w 4
+   \x 8
+   \y 4
+   \z 10})
+
 (defn score
-  [word])
+  [word]
+  (->> word
+       (map points)
+       (apply +)))
 
-
-(= 8 (score "hello"))
-
-(= 17 (score "question"))
-
-(= 35 (score "quizzed"))
+(score "saw")
