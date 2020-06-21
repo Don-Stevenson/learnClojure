f926e1209f72cef1a34b374fa8e75e348e5060fe
Don-Stevenson <dsteven1@gmail.com>
2020-06-21 14:22:30 -0400

    add instructions in comments

--- exercises/src/exercises/ex3.clj
+++ exercises/src/exercises/ex3.clj
@@ -1,8 +1,18 @@
+;;Write a function to determine if some 3 side lengths are sufficient to make a triangle.
+
+;;To check if 3 sides make a triangle, you need to check that every 
+;; side is less than or equal to the sum of the other two sides.
+
+
+;; returns true or false if three lengths can be made into a triangle
+;; ******************************************************************
 (defn triangle?
   [a b c]
   (and
    (< a (+ b c)) (< b (+ a c)) (< c (+ a b))))
 ;; returning if the triangle is not valid, equilateral, isosceles, right Triangle or scalene
+;;*******************************************************************************************
+
 (defn classifyTriangle
   [a b c]
   (cond (not (triangle? a b c)) :invalid
