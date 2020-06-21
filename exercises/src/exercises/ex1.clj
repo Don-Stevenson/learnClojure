c4622f5f9e9c6b4bf47cadea5f71605dd351e84c
Don-Stevenson <dsteven1@gmail.com>
2020-06-21 14:19:40 -0400

    add instructions in comments

--- exercises/src/exercises/ex1.clj
+++ exercises/src/exercises/ex1.clj
@@ -1,16 +1,20 @@
+;;Refactor the below code to use a single cond instead of multiple ifs.
+
+;;Also, consider re-ordering the conditionals to simplify the logic.
+
 ;(defn process-value
- ;; [value]
+ ; [value]
   ;(if (and (number? value) (> value 10))
    ; :pretty-big
-    ;(if (and (number? value) (< value 0))
-     ; :negative
-      ;(if (and (number? value) (zero? value))
-       ; :zero
-        ;(if (number? value)
-         ; :small-number
-          ;(if (string? value)
-           ; :a-string
-            ;:something-else))))))
+    ;;(if (and (number? value) (< value 0))
+      ;;:negative
+      ;;(if (and (number? value) (zero? value))
+        ;;:zero
+        ;;(if (number? value)
+          ;;:small-number
+          ;;(if (string? value)
+            ;;:a-string
+            ;;:something-else))))))
  
 (defn process-value 
   [value]
@@ -25,12 +29,4 @@
  :zero
  :else :small-number))   
 
-;;(defn process-value
-  ;;            [value]
-    ;;          (cond (string? value) :a-string
-      ;;              (not (number? value)) :something-else
-        ;;            (zero? value) :zero
-          ;;          (> value 10) :pretty-big
-            ;;        (< value 0) :negative
-              ;;      :else :small-number))
  (process-value 5)
