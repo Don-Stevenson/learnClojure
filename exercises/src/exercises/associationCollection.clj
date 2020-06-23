1bbdf56393afd86860ad13752a6f8195fbbc9e02
Don-Stevenson <dsteven1@gmail.com>
2020-06-23 14:18:12 -0400

    update comments

--- exercises/src/exercises/associationCollection.clj
+++ exercises/src/exercises/associationCollection.clj
@@ -1,8 +1,11 @@
-   
+;takes a key a value and data
+;uses assoc to change a key value 
+;to a new value and return a modified array
+;;*****************************************
+
    (defn get-and-set
      [key value data]
      [(data key)
-      (assoc data key value)] 
-     )
-   
-   (get-and-set 0 10 [1 2 3])     ;;=> [10 2 3]
+      (assoc data key value)])
+
+(get-and-set 0 10 [1 2 3])     ;;=> [1 [10 2 3]]
