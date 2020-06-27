b51bbe37d86cb103482355ce06d0a478636da7cc
Don-Stevenson <dsteven1@gmail.com>
2020-06-27 00:03:55 -0400

    update get and set, trying with an object

--- exercises/src/exercises/associationCollection.clj
+++ exercises/src/exercises/associationCollection.clj
@@ -3,9 +3,25 @@
 ;to a new value and return a modified array
 ;;*****************************************
 
-   (defn get-and-set
-     [key value data]
-     [(data key)
-      (assoc data key value)])
+   ;(defn get-and-set
+   ;  [key value data]
+  ;   [(data key)
+ ;     (assoc data key value)])
 
-(get-and-set 0 10 [1 2 3])     ;;=> [1 [10 2 3]]
+;(get-and-set 0 10 [1 2 3])     ;;=> [1 [10 2 3]]
+;
+;
+(defn get-and-set
+  [key, value, coll]
+  [;; gets the value that will be replaced
+   ;; 
+   (get coll key)
+   ;; replaces the original value at the key with a new value
+   (assoc coll key value)])
+
+;(get-and-set 0 2 [1 3 4])
+
+(get-and-set :a
+              "baz"
+              {:a "foo"
+               :b "quux"})
