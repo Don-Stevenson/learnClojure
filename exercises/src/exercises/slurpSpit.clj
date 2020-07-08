a37e8010d128f7337b1db146b2843d34e7fedfea
Don-Stevenson <dsteven1@gmail.com>
2020-07-08 13:52:18 -0400

    solution for slurp and spit code challenge

---
+++ exercises/src/exercises/slurpSpit.clj
@@ -0,0 +1,36 @@
+;; Read in a text file (assuming it's in the current directory),
+;; reverse the file's contents, and write the result to a new file (the name now prefixed by "rev-") .
+
+;;related functions
+;;slurp spit clojure.string/reverse str ->>
+;;
+;;
+;;
+;;;; ->> and -> by simple string concatenation
+;; Effectively (str " jmd" "hello")
+;;user=> (->> "hello" (str " jmd"))
+;;" jmdhello"
+;; Effectively (str "hello" " jmd")
+;;user=> (-> "hello" (str " jmd"))
+;;=> "hello jmd"
+
+;;Pair Programming solution with Elizabeth and Don
+;;************************************************
+(defn reverse-text 
+ [file]
+  (->> (slurp file)
+  (reverse)
+  (apply str)
+       (spit "rev-blurp.txt")))
+
+(reverse-text "text.txt")
+
+;; Given Solution
+;; **************
+(require '[clojure.string :as string])
+
+(defn reverse-file
+  [file-name]
+  (->> (slurp file-name)
+       string/reverse
+       (spit (str "rev-" file-name))))
