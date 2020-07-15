823864af02334910422c9e1321ca4195acf7c354
Don-Stevenson <dsteven1@gmail.com>
2020-07-15 13:28:47 -0400

    assoc in solution

--- exercises/src/exercises/assocIn.clj
+++ exercises/src/exercises/assocIn.clj
@@ -1,4 +1,5 @@
-;;Write a function that takes a map with this structure:
+;; Write a function that takes a map with this structure:
+;;******************************************************
 ;;
 ;;{:people {1 {:name "james"
 ;;             :points 1}
@@ -6,20 +7,37 @@
 ;;             :points 5}}}
 ;;as well as an id, a keyword, and some value.
 
-;;Return the same map but with the person with the given id having the given keyword & value added to their data (see tests for an example).
+;; Return the same map but with the person with the given id n
+;; having the given keyword & value added to their data (see tests for an example).
+
+(defn add-info
+  [obj-info id keyword val]
+  (assoc-in obj-info [:people id keyword] val))
+
+
+;; Given solution
+;; **************
+;;(defn add-info 
+;;  [info id key value] 
+;;  (assoc-in info [:people id key] value))
+
 
 ;;related functions
 ;;assoc-in
+;;
+;;
 ;;sample tests
-;;(is (= {:people {1 {:name "james"
-;;                    :points 1
-;;                    :stuff "beep boop"}
-;;                 2 {:name "rafd"
-;;                    :points 5}}}
-;;       (add-info {:people {1 {:name "james"
-;;                              :points 1}
-;;                           2 {:name "rafd"
-;;                              :points 5}}}
-;;                 1
-;;                 :stuff
-;;                 "beep boop")))
+;;************
+(= {:people {1 {:name "james"
+                   :points 1
+                   :stuff "beep boop"}
+                2 {:name "rafd"
+                   :points 5}}}
+      (add-info {:people {1 {:name "james"
+                              :points 1}
+                          2 {:name "rafd"
+                              :points 5}}}
+                 1
+                :stuff
+                 "beep boop"))
+
